0b92b74799550382eeb780dfe57b6b92c3ec8b1c
Diane Chen <dianedc@purpleweb.com>
2016-02-25 14:47:01 -0800

    Make index.html actually contain correct html

--- polls/views.py
+++ polls/views.py
@@ -16,12 +16,12 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
+        Return the last six published questions (not including those set to be
         published in the future).
         """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:6]
 
 
 class DetailView(generic.DetailView):
